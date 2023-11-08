5a7e017868ebf365b09e6b62056feb014f27d9e4
unknown <weisiyue071024@sjtu.edu.cn>
2023-11-08 09:38:13 +0800

    add microservice

---
+++ gateway/src/main/java/org/example/GatewayApplication.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(r -> r.path("/microservice/**")
+//						.filters(f -> f.rewritePath("/microservice",""))
+						.uri("lb://BOOK-SERVICE")
+				)
+				.route(r -> r.path("/api/**")
+						.uri("lb://MAIN-SERVICE")
+				)
+				.route(r -> r.path("/websocket/**")
+//						.filters(f -> f.rewritePath("/web",""))
+						.uri("lb://MAIN-SERVICE")
+				)
+				.build();
+	}
+}
